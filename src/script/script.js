64631770869e04536ef1980e7d9d2aa344da2b35
sotnaSelA <alessandro.santos.es@gmail.com>
2024-03-09 01:02:34 -0300

    Atualizando... Correção de bugs

--- src/script/script.js
+++ src/script/script.js
@@ -12,7 +12,7 @@ btn.addEventListener('click', function(){
 });
 
 
-var btn = document.querySelector('.hide');
+var btn = document.querySelector('#hide');
 var section = document.querySelector('.skills')
 var containe = document.querySelector('.skills');
 
@@ -25,48 +25,3 @@ btn.addEventListener('click', function(){
 
 });
 
-
-// Menu Mobile
-class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    animateLinks() {
-      this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-              index / 7 + 0.3
-            }s`);
-      });
-    }
-  
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
-    }
-  
-    addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
-  }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-  );
-  mobileNavbar.init();
